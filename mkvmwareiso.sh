c9f17c196e509c9bee0bae498a908d5ba430b90b
Mike Dollar <miketdollar@gmail.com>
2019-01-16 18:53:14 -0700

    Adding build script and ks config file.

---
+++ mkvmwareiso.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+
+set -uex
+
+DIR=$(dirname ${0})
+cd $DIR
+
+# Get ISO Image
+mkdir -p ISOTMP
+
+# You must have the Install ISO from oracle in same directory as the script
+if [ ! -f "V921569-01.iso" ]; then
+        echo "Original OL7 Install ISO must be in the same directory as this script"
+        exit 1
+fi
+
+cp V921569-01.iso ./ISOTMP/
+cd ISOTMP
+sudo mkdir -p /mnt/cdrom
+sudo mount -o loop V921569-01.iso /mnt/cdrom
+rm -rf image
+mkdir -p image
+rsync -av /mnt/cdrom/ image/
+sudo umount /mnt/cdrom
+
+chmod 755 image/
+cp ../minimum-ks.cfg image/ks.cfg
+
+cd image/isolinux/
+chmod 755 .
+chmod 644 isolinux.cfg
+sudo sed -i 's/  append initrd=initrd.img inst.stage2=hd:LABEL=OL-7.4\\x20Server.x86_64 rd.live.check quiet/  append initrd=initrd.img ks=cdrom:\/ks.cfg inst.txt quiet/' isolinux.cfg
+cd ../../
+sudo find image -type d -exec chmod 755 {} \;
+sudo find image -type f -exec chmod 644 {} \;
+
+NOW=$(date '+%Y%m%d-%H%M%S')
+ISO="truemark-vmware-ol-7-server-amd64-${NOW}.iso"
+
+mkisofs -r -V "Oracle Linux Install CD" \
+  -cache-inodes \
+  -J -l -b isolinux/isolinux.bin \
+  -c isolinux/boot.cat -no-emul-boot \
+  -boot-load-size 4 -boot-info-table \
+  -o ${ISO} image/
+
+sudo rm -rf image
+sha1sum ${ISO} > ${ISO}.sha1
+
